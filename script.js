f8eb895ac7becdd85b1cc8371e25595ccf53ea06
dostoynikov <145872991+dostoynikov@users.noreply.github.com>
2023-12-08 14:08:12 +0200

    add buttons with working values

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function getComputerChoice () {
-    let choiceNo = Math.floor(Math.random()*(4-1)+1);
-    switch(choiceNo) {
+
+
+
+function getComputerChoice() {
+    let choiceNo = Math.floor(Math.random() * (4 - 1) + 1);
+    switch (choiceNo) {
         case 1:
             return "rock"
             break;
@@ -16,50 +19,85 @@ function getComputerChoice () {
 let playerScore = 0;
 let computerScore = 0;
 
+let playerChoice = '';
 
-let playerSelection = prompt("rock or paper or scissors?");
-let computerSelection;
-
-function playRound (playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
+  
+    playerSelection = playerChoice;
 
     getComputerChoice();
     computerSelection = getComputerChoice();
 
     if (playerSelection == "rock" && computerSelection == "paper") {
         computerScore++;
+        console.log('computer wins with paper over rock')
         return "computer wins with paper over rock";
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         playerScore++;
+        console.log('player wins with rock over scissors')
         return "player wins with rock over scissors";
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         playerScore++;
+        console.log('player wins with paper over rock')
         return "player wins with paper over rock";
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         computerScore++;
+        console.log('computer wins with scissors over paper')
         return "computer wins with scissors over paper";
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         computerScore++;
+        console.log('computer wins with rock over scissors')
         return "computer wins with rock over scissors";
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         playerScore++;
+        console.log('player wins with scissors over paper')
         return "player wins with scissors over paper";
     } else {
+        console.log('tie')
         return "tie";
     };
 }
 
 
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
 
-function game() {
-    for (let i=0; i<5; i++) {
-    playRound(playerSelection, computerSelection);
-    
+rock.addEventListener('click', function() {
+    playerChoice = rock.value;
+    playRound();
+})
 
-}
-console.log(playerScore + " " + computerScore)
-playerScore = 0;
-computerScore = 0;
-}
+paper.addEventListener('click', function() {
+    playerChoice = paper.value;
+    playRound();
+})
+
+scissors.addEventListener('click', function() {
+    playerChoice = scissors.value;
+    playRound();
+})
 
 
-game()
+
+
+
+
+
+
+
+// function game() {
+//     for (let i=0; i<5; i++) {
+//     playRound(playerSelection, computerSelection);
+
+
+// }
+// console.log(playerScore + " " + computerScore)
+// playerScore = 0;
+// computerScore = 0;
+// }
+
+
+// game()
+
+
