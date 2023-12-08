f6912673667efd584b5a9b23d5cd66352b9b3eb7
dostoynikov <145872991+dostoynikov@users.noreply.github.com>
2023-12-08 12:10:28 +0200

    add prototype without ui

--- script.js
+++ script.js
@@ -13,22 +13,53 @@ function getComputerChoice () {
     }
 };
 
+let playerScore = 0;
+let computerScore = 0;
+
+
+let playerSelection = prompt("rock or paper or scissors?");
+let computerSelection;
+
 function playRound (playerSelection, computerSelection) {
-    if (playerSelection == "rock" || computerSelection == "paper") {
-        return "computer wins with paper over rock"
-    } else if (playerSelection == "rock" || computerSelection == "scissors") {
-        return "player wins with rock over scissors"
-    } else if (playerSelection == "paper" || computerSelection == "rock") {
-        return "player wins with paper over rock"
-    } else if (playerSelection == "paper" || computerSelection == "scissors") {
-        return "computer wins with scissors over paper"
-    } else if (playerSelection == "scissors" || computerSelection == "rock") {
-        return "computer wins with rock over scissors"
-    } else if (playerSelection == "scissors" || computerSelection == "paper") {
-        return "player wins with scissors over paper"
-    } else return "tie"
+
+    getComputerChoice();
+    computerSelection = getComputerChoice();
+
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        computerScore++;
+        return "computer wins with paper over rock";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        playerScore++;
+        return "player wins with rock over scissors";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        playerScore++;
+        return "player wins with paper over rock";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        computerScore++;
+        return "computer wins with scissors over paper";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        computerScore++;
+        return "computer wins with rock over scissors";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore++;
+        return "player wins with scissors over paper";
+    } else {
+        return "tie";
+    };
 }
 
-const computerSelection = getComputerChoice();
 
-console.log(playRound("paper", computerSelection))
+
+function game() {
+    for (let i=0; i<5; i++) {
+    playRound(playerSelection, computerSelection);
+    
+
+}
+console.log(playerScore + " " + computerScore)
+playerScore = 0;
+computerScore = 0;
+}
+
+
+game()
